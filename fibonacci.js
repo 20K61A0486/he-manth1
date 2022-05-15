87e75842e76fc0156ef346fb4480b5765d4106fb
hemanth <hemanthkumar486@sasi.ac.in>
2022-05-15 18:21:59 +0530

    assignment-1 solutions

--- fibonacci.js
+++ fibonacci.js
@@ -1,7 +1,10 @@
 let a=0,b=1,c;
                  console.log('fibonacci numbers upto 50 are')
-                 while(a<50){
-                     console.log(a);
+                 console.log(a);
+                 console.log(b);
+                 c=a+b;
+                 while(a<=50){
+                     console.log(c);
                      c=a+b;
                      a=b;
                      b=c;
